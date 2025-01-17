1a962d1f835eb1d38ebf4539b81687675ca9f39a
minseok127 <79395413+minseok127@users.noreply.github.com>
2025-01-17 13:43:59 +0900

    update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,1 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+set -u
+set -e
+set -x
+
+BASE_DIR=$DIR"/../"
+cd $BASE_DIR
+
+git submodule update --init --recursive
