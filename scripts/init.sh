3e30bceddc099d970670fafb1c2b9ec223e0b83b
minseok127 <yms127@naver.com>
2025-01-21 10:44:20 +0900

    modify init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,3 +11,21 @@ BASE_DIR=$DIR"/../"
 cd $BASE_DIR
 
 git submodule update --init --recursive
+
+sudo apt-get update
+
+sudo apt-get install \
+     build-essential \
+     ninja-build \
+     cmake
+
+ARROW_DIR=$BASE_DIR"/external/arrow/cpp"
+cd $ARROW_DIR
+
+rm -rf build
+mkdir build
+cd build
+
+cmake .. --preset ninja-release 
+cmake --build .
+cmake --install .
